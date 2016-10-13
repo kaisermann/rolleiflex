135447ee56fe62d896a41f3dbcada2505c1f4e4a
Christian Kaisermann <christian@kaisermann.me>
2016-10-13 15:43:15 -0300

    New version

--- gulpfile.js
+++ gulpfile.js
@@ -10,40 +10,38 @@ var browserString = 'last 2 versions';
 
 var endFile = (argv.endfile) ? argv.endfile : "rolleiflex";
 
-gulp.task('build', function() 
-{
-	var files = [['./src/build.styl','./dist/', endFile+'.css'], ['./page.styl','./','page.css']];
+gulp.task('build', function () {
+  var files = [
+    ['./src/build.styl', './dist/', endFile + '.css'],
+    ['./page.styl', './', 'page.css']
+  ];
 
-	var tasks = files.map(function(e)
-	{
-        return gulp.src(e[0])
-			.pipe($.plumber())
-			.pipe($.stylus())
-			.pipe($.autoprefixer(browserString))
-			.pipe($.stripCssComments())
-			.pipe($.rename(e[2]))
-			.pipe(gulp.dest(e[1]));
-    });
+  var tasks = files.map(function (e) {
+    return gulp.src(e[0])
+      .pipe($.plumber())
+      .pipe($.stylus())
+      .pipe($.autoprefixer(browserString))
+      .pipe($.stripCssComments())
+      .pipe($.rename(e[2]))
+      .pipe(gulp.dest(e[1]));
+  });
 
-    return merge(tasks);
+  return merge(tasks);
 });
 
-gulp.task('minify', ['build'], function()
-{
-	return gulp.src(['./dist/'+endFile+'.css'])
-	.pipe($.cssnano())
-	.pipe($.rename(endFile+'.min.css'))
-	.pipe(gulp.dest('./dist/'));
+gulp.task('minify', ['build'], function () {
+  return gulp.src(['./dist/' + endFile + '.css'])
+    .pipe($.cssnano())
+    .pipe($.rename(endFile + '.min.css'))
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', function() 
-{
-	gulp.watch(['src/*.styl','page.styl'], ['build', 'minify']);
+gulp.task('watch', function () {
+  gulp.watch(['src/*.styl', 'page.styl'], ['build', 'minify']);
 });
 
-gulp.task('clean', function()
-{
-	return gulp.src('./dist').pipe($.rimraf());
+gulp.task('clean', function () {
+  return gulp.src('./dist').pipe($.rimraf());
 });
 
 gulp.task('default', ['build', 'minify']);
